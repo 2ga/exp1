58c973ac6a84124b93ea0a847dd7d86604d8e5f8
Tester Hy1 <apache@www4013uj.sakura.ne.jp>
2013-01-09 20:12:43 +0900

    Issue #18

--- test3.php
+++ test3.php
@@ -3,7 +3,8 @@ for ($i=0; $i<100; $i++)
 {
     if ($i % 3 == 0) echo "<strong>Fiz</strong>";
     if ($i % 5 == 0) echo "<strong>Buz</strong>";
-    if ($i%3 !=0 %% $i%5 != 0) echo "<strong>{$i}</strong>";
+    if ($i%3 !=0 && $i%5 != 0) echo "<strong>{$i}</strong>";
     echo "<br />";
 }
+echo "<hr />";
 ?>
