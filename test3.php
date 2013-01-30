1231cbbbc82b87711f117818c39fb469f6157713
Apache <apache@www1358uj.sakura.ne.jp>
2013-01-30 15:43:13 +0900

    Issue #142

--- test3.php
+++ test3.php
@@ -2,7 +2,7 @@
 <body>
 <?php
 for ($i=1; $i<101; $i++){
- if($i%10 == 6 || (60 <= $i && $i <= 69)) {
+ if($i%10 == 4 || (40 <= $i && $i <= 49)) {
     echo "croak!croak!";
  }elseif($i%3 ==0 && $i%5 == 0){
     echo "Fizzbuzz";
