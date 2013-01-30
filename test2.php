d61ad67b6b69ec545309140ed5ae9fe51dc4e044
Tester Hy1 <apache@www4013uj.sakura.ne.jp>
2013-01-30 13:29:44 +0900

    Issue #123

--- test2.php
+++ test2.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $number=rand(1,10);
-if($number== 6 || $number==3 || $number==8 || $number==9 || $number==1 || $number==2){
+if($number==3 || $number==8 || $number==1 || $number==2){
   echo "Atari";
 }else{
   echo "Hazure";
